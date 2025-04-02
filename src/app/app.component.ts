04ac3544be3b41263a143e62f1186da19d36013d
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2025-04-02 19:37:40 -0300

    criação do card pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./components/card/card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, CardComponent],
+  template: `
+    <h1>Teste de Services</h1>
+    <app-card [name]="'CHARMANDER'" [attributesTypes]="['FIRE', 'FLY']"></app-card>
+    <router-outlet></router-outlet>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
